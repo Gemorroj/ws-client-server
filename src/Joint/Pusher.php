6f08fe1fa7b0aa25e9df7ab0170fe3ef68d1c603
Gemorroj <wapinet@gmail.com>
2018-03-09 18:20:05 +0300

    restore database connection

--- src/Joint/Pusher.php
+++ src/Joint/Pusher.php
@@ -18,9 +18,13 @@ class Pusher implements MessageComponentInterface
     public function __construct(int $interval = 5000)
     {
         $this->interval = $interval;
-        $pool = \Amp\Mysql\pool("host=127.0.0.1 user=root password= db=test");
+
+        echo 'Connect to database' . "\n";
+        $pool = \Amp\Mysql\pool('host=127.0.0.1 user=root password= db=test');
+
         \Amp\asyncCall(function () use ($pool) {
-            $this->stmtUserData = yield $pool->prepare("SELECT * FROM user WHERE id = ?");
+            echo 'Prepare database statement' . "\n";
+            $this->stmtUserData = yield $pool->prepare('SELECT * FROM user WHERE id = ?');
         });
     }
 
@@ -48,19 +52,24 @@ class Pusher implements MessageComponentInterface
             echo 'Start timer for client ' . $conn->resourceId . "\n";
 
             $this->clients[$conn->resourceId]['timer'] = \Amp\Loop::repeat($this->interval, function () use ($userMsg, $conn) {
-                /** @var \Amp\Mysql\ResultSet $result */
-                $result = yield $this->stmtUserData->execute([$userMsg->userId]);
+                try {
+                    /** @var \Amp\Mysql\ResultSet $result */
+                    $result = yield $this->stmtUserData->execute([$userMsg->userId]);
 
-                while (yield $result->advance()) {
-                    $row = $result->getCurrent();
+                    while (yield $result->advance()) {
+                        $row = $result->getCurrent();
 
-                    $msg = \json_encode(new EmitterMessage(
-                        'message',
-                        $conn->resourceId,
-                        \json_encode($row)
-                    ));
-                    echo 'Send message to client ' . $conn->resourceId . ': ' . $msg . "\n";
-                    $conn->send($msg);
+                        $msg = \json_encode(new EmitterMessage(
+                            'message',
+                            $conn->resourceId,
+                            \json_encode($row)
+                        ));
+                        echo 'Send message to client ' . $conn->resourceId . ': ' . $msg . "\n";
+                        $conn->send($msg);
+                    }
+                } catch (\Amp\Mysql\ConnectionException $e) { // auto reconnect
+                    echo 'Lost database connection: ' . $e->getMessage(). "\n";
+                    $this->__construct($this->interval);
                 }
             });
         }
